3e708738d4558791a77b2ec4968008ed91e5d57a
Me <rubikone89pw@rambler.ru>
2024-07-20 23:39:51 +0300

    debug

--- src/main/java/Bank/model/Beneficiary.java
+++ src/main/java/Bank/model/Beneficiary.java
@@ -8,13 +8,23 @@ import org.springframework.data.annotation.Id;
 @Entity
 public class Beneficiary {
 
+    @jakarta.persistence.Id
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
+    private String accountNumber;
 
-    // Getters and setters
+    // Конструкторы, геттеры и сеттеры
+    public Beneficiary() {
+    }
+
+    public Beneficiary(String name, String accountNumber) {
+        this.name = name;
+        this.accountNumber = accountNumber;
+    }
+
     public Long getId() {
         return id;
     }
@@ -30,4 +40,20 @@ public class Beneficiary {
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
 }
